08d12476e8ee0714caaa79f55abe72a23d31bdff
Abraham Lira Medina <abraham_.lir@hotmail.com>
2016-10-17 00:43:47 -0500

    Agrega funcionalidad resta, multiplicacion, divicion y pruebas con lettuce Para probar la funcionalidad desde el punto de vista del usuario, se integran pruebas de aceptación utilizando lettuce.

--- calculadora/tests/features/steps.py
+++ calculadora/tests/features/steps.py
@@ -5,17 +5,34 @@ from calculadora import Calculadora
 
 @step(u'dado que tengo los operadores "([^"]*)" y "([^"]*)"')
 def dado_que_tengo_los_operadores_group1_y_group1(step, num1, num2):
-    cal = Calculadora()
-    world.resultado = cal.suma(int(num1), int(num2))
+    world.operador1 = int(num1)
+    world.operador2 = int(num2)
+    world.cal = Calculadora()
 
 
 @step(u'cuando realizo la suma')
 def cuando_realizo_la_suma(step):
-    pass
+    world.operacion = world.cal.suma(world.operador1, world.operador2)
 
 
 @step(u'entonces el resultado que obtengo es "([^"]*)"')
-def entonces_el_resultado_que_obtengo_es_group1(step, esperado):
-    assert int(esperado) == world.resultado,\
-        'El resultado esperado no es el mismo' + \
-        esperado + 'no es' + str(world.resultado)
+def entonces_el_resultado_que_obtengo_es_group1(step, resultado):
+    assert world.operacion == int(
+        resultado), 'El resultado  esperado no es el mismo' +\
+        resultado + 'no es' + str(world.operacion)
+
+
+@step(u'cuando realizo la resta')
+def cuando_realizo_la_resta(step):
+    world.operacion = world.cal.resta(world.operador1, world.operador2)
+
+
+@step(u'cuando realizo la multiplicacion')
+def cuando_realizo_la_multiplicacion(step):
+    world.operacion = world.cal.multiplicacion(
+        world.operador1, world.operador2)
+
+
+@step(u'cuando realizo la divicion')
+def cuando_realizo_la_divicion(step):
+    world.operacion = world.cal.divicion(world.operador1, world.operador2)
--- calculadora/tests/features/suma.feature
+++ calculadora/tests/features/suma.feature
@@ -10,4 +10,19 @@ Feature: Suma de dos numeros
 		cuando realizo la suma 
 		entonces el resultado que obtengo es "4"
 
+	Scenario: Restar 2 menos 1
+		dado que tengo los operadores "2" y "1"
+		cuando realizo la resta 
+		entonces el resultado que obtengo es "1"
+
+	Scenario: Multiplicacion 5 menos 5
+		dado que tengo los operadores "5" y "5"
+		cuando realizo la multiplicacion 
+		entonces el resultado que obtengo es "25"
+
+	Scenario: Divicion 100 menos 5
+		dado que tengo los operadores "100" y "5"
+		cuando realizo la divicion
+		entonces el resultado que obtengo es "20"
+
 
